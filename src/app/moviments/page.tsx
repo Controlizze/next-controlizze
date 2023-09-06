2116ec04f5865393d4ec66946ecc9ead0d75b1f8
souorichard <elaine.ra96@gmail.com>
2023-09-05 22:44:28 -0300

    feat: alterações básicas

--- src/app/moviments/page.tsx
+++ src/app/moviments/page.tsx
@@ -7,9 +7,9 @@ import {
   LuMenu,
   LuWallet
 } from 'react-icons/lu'
+
 import { Sidebar } from '../../components/Sidebar/index'
 import { Table } from './components/Table'
-
 
 export const metadata = {
   title: 'Dashboard - Controlizze'
@@ -38,190 +38,190 @@ export default function MovimentsPage() {
         </header>
       )}
 
-      <header className="fixed w-full h-20 px-5 flex items-center gap-2 bg-background-700 shadow-xl shadow-default z-20 hidden md:flex">
+      <header className="fixed w-full h-20 px-5 hidden items-center gap-2 bg-background-700 shadow-xl shadow-default z-20">
         <div className="w-fit flex flex-col justify-center gap-0">
           <h3 className="text-xs text-zinc-400 uppercase">Dashboard</h3>
           <h1 className="text-xl font-bold text-white">Movimentações</h1>
         </div>
       </header>
 
-      {(sidebarOpen && (
+      <main className="w-full px-7 py-5 mt-20 flex flex-1 flex-col gap-8">
+        {/*Cards*/}
+        <div className="flex flex-col gap-4">
+          <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-0.5">
+                <h3 className="text-lg font-medium text-white">Despesas</h3>
+                <div
+                  className={`w-3/5 h-1 bg-gradient-to-r from-red-700 to-background-700 rounded-full`}
+                ></div>
+              </div>
+              <LuArrowDownCircle className="text-3xl text-red-700" />
+            </div>
+            <h2 className="text-3xl font-bold text-white text-center">
+              R$0.00
+            </h2>
+          </div>
+
+          <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-0.5">
+                <h3 className="text-lg font-medium text-white">Receitas</h3>
+                <div
+                  className={`w-3/5 h-1 bg-gradient-to-r from-green-700 to-background-700 rounded-full`}
+                ></div>
+              </div>
+              <LuArrowUpCircle className="text-3xl text-green-700" />
+            </div>
+            <h2 className="text-3xl font-bold text-white text-center">
+              R$0.00
+            </h2>
+          </div>
+
+          <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-0.5">
+                <h3 className="text-lg font-medium text-white">Saldo</h3>
+                <div
+                  className={`w-3/5 h-1 bg-gradient-to-r from-orange-700 to-background-700 rounded-full`}
+                ></div>
+              </div>
+              <LuWallet className="text-3xl text-orange-700" />
+            </div>
+            <h2 className="text-3xl font-bold text-white text-center">
+              R$0.00
+            </h2>
+          </div>
+        </div>
+
+        {/*Card*/}
+        <div className="w-full p-5 flex flex-col gap-7 bg-background-700 rounded shadow-xl shadow-default">
+          <div className="flex items-center gap-7">
+            <h3 className="text-xl font-semibold text-white">
+              Incluir Registro
+            </h3>
+            <div
+              className={`h-1 flex flex-auto bg-gradient-to-r from-primary-500 to-background-700 rounded-full`}
+            ></div>
+          </div>
+
+          <div className="w-full flex flex-col gap-4">
+            <fieldset className="w-full flex flex-col gap-0.5">
+              <label
+                htmlFor="date"
+                className="text-xs font-medium text-zinc-400"
+              >
+                Informe a data
+              </label>
+              <div className="relative w-full flex">
+                <input
+                  id="date"
+                  type="text"
+                  className="w-full h-full px-3 py-2 rounded outline-none"
+                  placeholder="dd/mm/yyyy"
+                />
+              </div>
+            </fieldset>
+
+            <fieldset className="w-full flex flex-col gap-0.5">
+              <label
+                htmlFor="description"
+                className="text-xs font-medium text-zinc-400"
+              >
+                Informe a descrição
+              </label>
+              <div className="relative w-full flex">
+                <input
+                  id="description"
+                  type="text"
+                  className="w-full h-full px-3 py-2 rounded outline-none"
+                  placeholder="Descrição"
+                />
+              </div>
+            </fieldset>
+
+            <fieldset className="w-full flex flex-col gap-0.5">
+              <label
+                htmlFor="category"
+                className="text-xs font-medium text-zinc-400"
+              >
+                Informe a categoria
+              </label>
+              <div className="relative w-full flex">
+                <input
+                  id="category"
+                  type="text"
+                  className="w-full h-full px-3 py-2 rounded outline-none"
+                  placeholder="Categoria"
+                />
+              </div>
+            </fieldset>
+
+            <fieldset className="w-full flex flex-col gap-0.5">
+              <label
+                htmlFor="value"
+                className="text-xs font-medium text-zinc-400"
+              >
+                Informe o valor
+              </label>
+              <div className="relative w-full flex">
+                <input
+                  id="value"
+                  type="text"
+                  className="w-full h-full px-3 py-2 rounded outline-none"
+                  placeholder="Valor"
+                />
+              </div>
+            </fieldset>
+          </div>
+
+          <div className="flex px-4 justify-between items-center">
+            <fieldset className="flex items-center gap-1">
+              <input
+                id="type-expense"
+                type="radio"
+                name="type"
+                className="w-4 h-4 accent-primary-500"
+              />
+              <label htmlFor="type-expense" className="text-base text-white">
+                Despesa
+              </label>
+            </fieldset>
+
+            <fieldset className="flex items-center gap-1">
+              <input
+                id="type-revenue"
+                type="radio"
+                name="type"
+                className="w-4 h-4 accent-primary-500"
+              />
+              <label htmlFor="type-revenue" className="text-base text-white">
+                Receita
+              </label>
+            </fieldset>
+          </div>
+
+          <button className="h-12 px-3 flex justify-center items-center bg-primary-500 hover:opacity-75 rounded shadow-lg shadow-default transition-all text-sm font-semibold uppercase">
+            Adicionar
+          </button>
+        </div>
+
+        {/*Card*/}
+        <div className="w-full p-5 flex flex-col gap-7 bg-background-700 rounded shadow-xl shadow-default">
+          <div className="flex items-center gap-7">
+            <h3 className="text-xl font-semibold text-white">Registros</h3>
+            <div
+              className={`h-1 flex flex-auto bg-gradient-to-r from-primary-500 to-background-700 rounded-full`}
+            ></div>
+          </div>
+
+          <Table />
+        </div>
+      </main>
+
+      {/* {(sidebarOpen && (
         <div className="w-screen h-screen  bg-background-700 z-[30]"></div>
-      )) || (
-        <main className="w-full px-7 py-5 mt-20 flex flex-1 flex-col gap-8">
-          {/*Cards*/}
-          <div className="flex flex-col gap-4">
-            <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-0.5">
-                  <h3 className="text-lg font-medium text-white">Despesas</h3>
-                  <div
-                    className={`w-3/5 h-1 bg-gradient-to-r from-red-700 to-background-700 rounded-full`}
-                  ></div>
-                </div>
-                <LuArrowDownCircle className="text-3xl text-red-700" />
-              </div>
-              <h2 className="text-3xl font-bold text-white text-center">
-                R$0.00
-              </h2>
-            </div>
-
-            <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-0.5">
-                  <h3 className="text-lg font-medium text-white">Receitas</h3>
-                  <div
-                    className={`w-3/5 h-1 bg-gradient-to-r from-green-700 to-background-700 rounded-full`}
-                  ></div>
-                </div>
-                <LuArrowUpCircle className="text-3xl text-green-700" />
-              </div>
-              <h2 className="text-3xl font-bold text-white text-center">
-                R$0.00
-              </h2>
-            </div>
-
-            <div className="w-full px-5 py-3 flex flex-col gap-2 bg-background-700 rounded shadow-xl shadow-default">
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-0.5">
-                  <h3 className="text-lg font-medium text-white">Saldo</h3>
-                  <div
-                    className={`w-3/5 h-1 bg-gradient-to-r from-orange-700 to-background-700 rounded-full`}
-                  ></div>
-                </div>
-                <LuWallet className="text-3xl text-orange-700" />
-              </div>
-              <h2 className="text-3xl font-bold text-white text-center">
-                R$0.00
-              </h2>
-            </div>
-          </div>
-
-          {/*Card*/}
-          <div className="w-full p-5 flex flex-col gap-7 bg-background-700 rounded shadow-xl shadow-default">
-            <div className="flex items-center gap-7">
-              <h3 className="text-xl font-semibold text-white">
-                Incluir Registro
-              </h3>
-              <div
-                className={`h-1 flex flex-auto bg-gradient-to-r from-primary-500 to-background-700 rounded-full`}
-              ></div>
-            </div>
-
-            <div className="w-full flex flex-col gap-4">
-              <fieldset className="w-full flex flex-col gap-0.5">
-                <label
-                  htmlFor="date"
-                  className="text-xs font-medium text-zinc-400"
-                >
-                  Informe a data
-                </label>
-                <div className="relative w-full flex">
-                  <input
-                    id="date"
-                    type="text"
-                    className="w-full h-full px-3 py-2 rounded outline-none"
-                    placeholder="dd/mm/yyyy"
-                  />
-                </div>
-              </fieldset>
-
-              <fieldset className="w-full flex flex-col gap-0.5">
-                <label
-                  htmlFor="description"
-                  className="text-xs font-medium text-zinc-400"
-                >
-                  Informe a descrição
-                </label>
-                <div className="relative w-full flex">
-                  <input
-                    id="description"
-                    type="text"
-                    className="w-full h-full px-3 py-2 rounded outline-none"
-                    placeholder="Descrição"
-                  />
-                </div>
-              </fieldset>
-
-              <fieldset className="w-full flex flex-col gap-0.5">
-                <label
-                  htmlFor="category"
-                  className="text-xs font-medium text-zinc-400"
-                >
-                  Informe a categoria
-                </label>
-                <div className="relative w-full flex">
-                  <input
-                    id="category"
-                    type="text"
-                    className="w-full h-full px-3 py-2 rounded outline-none"
-                    placeholder="Categoria"
-                  />
-                </div>
-              </fieldset>
-
-              <fieldset className="w-full flex flex-col gap-0.5">
-                <label
-                  htmlFor="value"
-                  className="text-xs font-medium text-zinc-400"
-                >
-                  Informe o valor
-                </label>
-                <div className="relative w-full flex">
-                  <input
-                    id="value"
-                    type="text"
-                    className="w-full h-full px-3 py-2 rounded outline-none"
-                    placeholder="Valor"
-                  />
-                </div>
-              </fieldset>
-            </div>
-
-            <div className="flex px-4 justify-between items-center">
-              <fieldset className="flex items-center gap-1">
-                <input
-                  id="type-expense"
-                  type="radio"
-                  name="type"
-                  className="w-4 h-4 accent-primary-500"
-                />
-                <label htmlFor="type-expense" className="text-base text-white">
-                  Despesa
-                </label>
-              </fieldset>
-
-              <fieldset className="flex items-center gap-1">
-                <input
-                  id="type-revenue"
-                  type="radio"
-                  name="type"
-                  className="w-4 h-4 accent-primary-500"
-                />
-                <label htmlFor="type-revenue" className="text-base text-white">
-                  Receita
-                </label>
-              </fieldset>
-            </div>
-
-            <button className="h-12 px-3 flex justify-center items-center bg-primary-500 hover:opacity-75 rounded shadow-lg shadow-default transition-all text-sm font-semibold uppercase">
-              Adicionar
-            </button>
-          </div>
-
-          {/*Card*/}
-          <div className="w-full p-5 flex flex-col gap-7 bg-background-700 rounded shadow-xl shadow-default">
-            <div className="flex items-center gap-7">
-              <h3 className="text-xl font-semibold text-white">Registros</h3>
-              <div
-                className={`h-1 flex flex-auto bg-gradient-to-r from-primary-500 to-background-700 rounded-full`}
-              ></div>
-            </div>
-
-            <Table />
-          </div>
-        </main>
-      )}
+      )) || ()} */}
     </div>
   )
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,89 +1,79 @@
 'use client'
 
-import { links } from 'data/mocks'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { LuLogOut, LuXCircle } from 'react-icons/lu'
 
+import { links } from 'data/mocks'
 
 export function Sidebar() {
   const pathname = usePathname()
-
-  function isLoginOrRegister() {
-    return (
-      !pathname.includes('login') &&
-      !pathname.includes('register') &&
-      pathname !== '/'
-    )
-  }
 
   const linkClass =
     'px-4 py-3.5 flex items-center gap-2 bg-transparent data-[selected=true]:bg-background-500 border-l-4 border-background-500 data-[selected=true]:border-primary-500 rounded hover:opacity-75 transition-all shadow-md text-white'
 
   return (
-    (isLoginOrRegister() && (
-      <aside className="abosolute top-0 left-0 w-full h-screen flex flex-col bg-background-700 z-20">
-        <div className="flex flex-auto flex-col p-7 gap-16">
-          <div className="flex justify-between items-center">
-            <div className="flex justify-center items-center gap-2">
-              <Image src={'/icon.png'} width={28} height={28} alt="emblem" />
-              <span
-                style={{ fontFamily: 'Russo One' }}
-                className="text-3xl text-white"
-              >
-                Controlizze
-              </span>
-            </div>
-
-            <button>
-              <LuXCircle className="w-6 h-6 text-white" />
-            </button>
-          </div>
-
-          <div className="flex flex-col gap-8">
-            {links.map((item) => (
-              <div key={item.id} className="flex flex-col gap-5">
-                <span className="text-base font-semibold text-zinc-500 uppercase">
-                  {item.title}
-                </span>
-                <div className="flex flex-col gap-5">
-                  {item.links.map((link) => (
-                    <Link
-                      key={link.id}
-                      href={`/${link.path}`}
-                      data-selected={pathname.includes(link.path)}
-                      className={linkClass}
-                    >
-                      {link.icon}
-                      {link.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="h-20 px-7 flex justify-between items-center bg-background-500">
+    <aside className="abosolute top-0 left-0 w-full h-screen flex flex-col bg-background-700 z-20">
+      <div className="flex flex-auto flex-col p-7 gap-16">
+        <div className="flex justify-between items-center">
           <div className="flex justify-center items-center gap-2">
-            <Image
-              src="/user_photo.jfif"
-              width={36}
-              height={36}
-              alt="user_image"
-              className="rounded-full"
-            />
-            <span className="text-sm text-white">
-              Olá, <span className="font-semibold">Richard</span>
+            <Image src={'/icon.png'} width={28} height={28} alt="emblem" />
+            <span
+              style={{ fontFamily: 'Russo One' }}
+              className="text-3xl text-white"
+            >
+              Controlizze
             </span>
           </div>
 
-          <button className="w-7 h-7 flex justify-center items-center border border-primary-500 rounded-full">
-            <LuLogOut className="w-4 h-4 text-primary-500" />
+          <button>
+            <LuXCircle className="w-6 h-6 text-white" />
           </button>
         </div>
-      </aside>
-    )) || <></>
+
+        <div className="flex flex-col gap-8">
+          {links.map((item) => (
+            <div key={item.id} className="flex flex-col gap-5">
+              <span className="text-base font-semibold text-zinc-500 uppercase">
+                {item.title}
+              </span>
+              <div className="flex flex-col gap-5">
+                {item.links.map((link) => (
+                  <Link
+                    key={link.id}
+                    href={`/${link.path}`}
+                    data-selected={pathname.includes(link.path)}
+                    className={linkClass}
+                  >
+                    {link.icon}
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="h-20 px-7 flex justify-between items-center bg-background-500">
+        <div className="flex justify-center items-center gap-2">
+          <Image
+            src="/user_photo.jfif"
+            width={36}
+            height={36}
+            alt="user_image"
+            className="rounded-full"
+          />
+          <span className="text-sm text-white">
+            Olá, <span className="font-semibold">Richard</span>
+          </span>
+        </div>
+
+        <button className="w-7 h-7 flex justify-center items-center border border-primary-500 rounded-full">
+          <LuLogOut className="w-4 h-4 text-primary-500" />
+        </button>
+      </div>
+    </aside>
   )
 }
